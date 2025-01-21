8dcc03df4f090672b41668bf37eacf8d36c46ae8
exiong02 <ericex134@gmail.com>
2025-01-21 04:06:04 -0800

    Setup GitHub Pages deployment with new workflow

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: {
-    unoptimized: true,
-  },
   basePath: "/xeex.github.io",
-  assetPrefix: "/xeex.github.io",
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
