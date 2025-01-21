b9063e55ca405048215e67cafb4d3644777ea9ec
exiong02 <ericex134@gmail.com>
2025-01-21 04:51:00 -0800

    Remove Sentry API route for static export compatibility

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
-  basePath: isProduction ? "/xeex.github.io" : "",
-  assetPrefix: isProduction ? "/xeex.github.io/" : "",
+  basePath: "/xeex.github.io",
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
